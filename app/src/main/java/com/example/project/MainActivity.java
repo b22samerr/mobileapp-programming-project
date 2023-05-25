8ce2bd12ca3cb645269dc75cca7dd6b5bf76ab33
b22samer <b22samer@student.his.se>
2023-05-25 10:45:09 +0200

    Skapar en Arraylista samt var tvungen att använda JSON_FILE istället för JSON_URL

--- app/src/main/java/com/example/project/MainActivity.java
+++ app/src/main/java/com/example/project/MainActivity.java
@@ -5,42 +5,26 @@ import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
-import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
-
-import com.google.gson.*;
-import com.google.gson.reflect.*;
 
 import java.lang.reflect.Type;
 import java.util.ArrayList;
 import java.util.List;
 
-import android.content.Intent;
-import android.os.Bundle;
-import android.util.Log;
-import android.view.View;
-import android.widget.Button;
-
-import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.Toolbar;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
 
-import java.lang.reflect.Type;
-import java.util.List;
-import java.util.ArrayList;
-
 public class MainActivity extends AppCompatActivity implements JsonTask.JsonTaskListener {
 
     private final String JSON_URL = "https://mobprog.webug.se/json-api?login=b22samer";
+    private final String JSON_FILE = "soccerteams.json";
     private RecyclerView recyclerView;
-    private List<FootballTeams> FootballTeamsList;
+    private ArrayList<FootballTeams> footballTeamsList;
     private RecyclerViewAdapter adapter;
     private Button button;
 
@@ -51,11 +35,13 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        new JsonTask(this).execute(JSON_URL);
+        footballTeamsList = new ArrayList<>();
+
+
 
         recyclerView = findViewById(R.id.recycler_view);
-        FootballTeamsList = new ArrayList<FootballTeams>();
-        adapter = new RecyclerViewAdapter(FootballTeamsList);
+
+        adapter = new RecyclerViewAdapter(footballTeamsList);
         recyclerView.setAdapter(adapter);
         recyclerView.setLayoutManager(new LinearLayoutManager(this));
         button = findViewById(R.id.button);
@@ -67,14 +53,18 @@ public class MainActivity extends AppCompatActivity implements JsonTask.JsonTask
                 startActivity(intent);
             }
         });
+
+        //new JsonTask(this).execute(JSON_URL);
+        new JsonFile(this,this).execute(JSON_FILE);
     }
 
     @Override
     public void onPostExecute(String json) {
+        Log.d("Samuel", "onPostExecute: "+json);
         Gson gson = new Gson();
         Type type = new TypeToken<ArrayList<FootballTeams>>(){}.getType();
         ArrayList<FootballTeams> data = gson.fromJson(json, type);
-        FootballTeamsList.addAll(data);
-        adapter.notifyDataSetChanged();
+        footballTeamsList.addAll(data);
+        adapter.addData(footballTeamsList);
     }
 }
